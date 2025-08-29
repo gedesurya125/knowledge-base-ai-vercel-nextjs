7a0a4788f9d965989ade1fcc77fa68a380a340b0
gedesurya125 <gedesurya125@gmail.com>
2025-08-30 00:46:55 +0800

    fix the spinner color and the stroke width

--- components/icon/Spinner.tsx
+++ components/icon/Spinner.tsx
@@ -6,7 +6,7 @@ export const Spinner = ({
 }: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg
-      className={` size-5 animate-spin text-white ${className}`}
+      className={` size-5 animate-spin ${className}`}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -18,7 +18,7 @@ export const Spinner = ({
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
