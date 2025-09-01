a41d748ea86931f1bc3be76c4daee417aee8c7cc
gedesurya125 <gedesurya125@gmail.com>
2025-09-01 18:21:06 +0800

    touch up the var naming

--- app/page.tsx
+++ app/page.tsx
@@ -90,11 +90,11 @@ export default function Chat() {
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <div className="space-y-4">
-        {messages.map((m) => (
-          <div key={m.id} className="whitespace-pre-wrap">
+        {messages.map((messageItem) => (
+          <div key={messageItem.id} className="whitespace-pre-wrap">
             <div>
-              <div className="font-bold">{m.role}</div>
-              {m.parts.map((part) => {
+              <div className="font-bold">{messageItem.role}</div>
+              {messageItem.parts.map((part) => {
                 switch (part.type) {
                   case "text":
                     return <p>{part.text}</p>;
